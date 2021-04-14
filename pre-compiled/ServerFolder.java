166980d73b5b60a3eb604a9d02b88bec8cff7e49
DomChristie15 <80075638+DomChristie15@users.noreply.github.com>
2021-04-14 13:40:58 +1000

    Update ServerFolder.java
    Just moving the file from the previous folder, no changes other than the comment at the top

--- pre-compiled/ServerFolder.java
+++ pre-compiled/ServerFolder.java
@@ -1,19 +1,20 @@
+// server class to initialize the server properties such as corecount, memory, etc
 public class ServerFolder {
 	public int id;
 	public String type;
 	public int limit;
 	public int bootupTime;
-	public float rate;
+	public float hourlyRate;
 	public int coreCount;
 	public int memory;
 	public int disk;
 
-	public Server(int id, String t, int l, int b, float r, int c, int m, int d) {
+	public ServerFolder(int id, String t, int l, int b, float r, int c, int m, int d) {
 		this.id = id;
 		this.type = t;
 		this.limit = l;
 		this.bootupTime = b;
-		this.rate = r;
+		this.hourlyRate = r;
 		this.coreCount = c;
 		this.memory = m;
 		this.disk = d;
