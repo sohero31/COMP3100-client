5f4f80e5a35f82a0b4a28400dcadea90e4f48f6b
DomChristie15 <80075638+DomChristie15@users.noreply.github.com>
2021-04-18 11:51:23 +1000

    Update Client.java
    I have made a number of changes. These include changing .read to .readLine at line 110, adding \n to all the messages sent to the server and I have changed the Auth to reflect the username of my system. All of these changes were necessary for the client to pass the provided tests. I also pre-initialised the jobData string array to ensure that I don't get out of bounds exceptions.

--- pre-compiled/Client.java
+++ pre-compiled/Client.java
@@ -7,19 +7,20 @@ import org.w3c.dom.*;
 import Folder.Server;	
 
 public class Client {
-
 	//Initialising the socket, the input/output stream, and the msg string
 	//which will allow to send/receive messages.
 	Socket s = null;
 	String msg = "";
-	
 	//I have read that DataInputStream is outdated/deprecated 
 	//and I have found a way to use buffered reader in a way 
 	//that works just as well
 	BufferedReader din = null;
 	DataOutputStream dout = null;
+	Boolean flag = false;
 	List<Server> sArr  = new ArrayList<Server>();
 	int largeServer  = 0;
+	
+	
 	
 
 
@@ -35,31 +36,32 @@ public class Client {
 	
 	
 	public void Communicator() {
-	//this small section is the pseudo-authentication between server and client.
-		messageSend("HELO");
+		//this is the pseudo-authentication betweeen server and client
+		messageSend("HELO\n");
 		msg = messageReceive();
-		messageSend("AUTH Comp3100");
+		messageSend("AUTH dom\n");
 		msg = messageReceive();
-	//upon succesful auth, the client will receive the XML and parse it
 		parse();
-		messageSend("REDY");
+		messageSend("REDY\n");
 		msg = messageReceive();
 		
 
-	//this section relates to the actual scheduling of tasks/jobs and dealing with JCPL through an if/else conditional
+		//this is the section that schedules jobs
 		while (!msg.equals("NONE")) {
 			if (msg.equals("OK")) {
-					messageSend("REDY");
+					messageSend("REDY\n");
 					msg = messageReceive();
 				}
 			
-			String[] jobData = msg.split("\\s+");	
+			String[] jobData = new String[10];
+			jobData = msg.split("\\s+");
 			int count = Integer.parseInt(jobData[2]);
-			if (!jobData[0].equals("JCPL")) {
-				messageSend("SCHD "+count+" "+sArr.get(largeServer).type + " " + "0");
+			if (!jobData[0].equals("JCPL")) {	
+				
+				messageSend("SCHD "+count+" "+sArr.get(largeServer).type + " " + "0\n");
 				msg = messageReceive();
 			} else {
-				messageSend("REDY");
+				messageSend("REDY\n");
 				msg = messageReceive();
 			}
 				
@@ -74,7 +76,7 @@ public class Client {
 	//server closes
 	public void quitCommunicating() {
 		try {
-			messageSend("QUIT");
+			messageSend("QUIT\n");
 			msg = messageReceive();
 			if (msg == "QUIT") {
 				din.close();
@@ -85,7 +87,6 @@ public class Client {
 			System.out.println(i);
 		}
 	}
-	
 	//sending a message using the DataOutputStream 
 	//and then flushing it. Also added in an exception handler
 	//this code is the same as before, just wrapped in a function
@@ -98,7 +99,6 @@ public class Client {
 				System.out.println(e);
 			}
 	}
-	
 	//receiving a message and returning a string.
 	public String messageReceive() {
 		String message = "";
@@ -107,7 +107,7 @@ public class Client {
 			while (!din.ready()){}
 			//if it is ready, then receive the data.
 			while (din.ready()) {
-			message += (char) din.read();
+			message += din.readLine();
 		}
 		msg = message;
 		//Added in an exception handler, kept getting errors 
@@ -117,8 +117,6 @@ public class Client {
 		}
 		return message;
 	}
-	
-	// Finds the largest Server and returns it
 	public int largeServer() {
 		int largeServer = sArr.get(0).id;
 		for (int i=1; i < sArr.size(); i++) {
@@ -128,8 +126,6 @@ public class Client {
 		}
 		return largeServer;
 	}
-	
-	//Parses the xml file
 	public void parse(){
         try{
             File sysXML = new File("ds-system.xml");
@@ -152,6 +148,7 @@ public class Client {
 				int d = Integer.parseInt(server.getAttribute("disk"));
 				Server temp = new Server(i, t, l, b, r, c, m, d);	//initialising data server temp to contain all the nodes 
 				sArr.add(temp);					
+				//System.out.println(sArr.get(sArr.size()-1).coreCount);			//printing out the number of cores
 			}
 			largeServer = largeServer();
 		} catch (Exception ex) {
@@ -159,18 +156,16 @@ public class Client {
 		}
 
     }
-    
 	//trying to use constructors and functions
 	//instead of big chunks of code
 	public static void main(String[] args) {
-		//this establishes a new instance of the Client
-		//named client and then runs the communicator function
+		//this establishes a new instance of the ClientTest
+		//name client and then runs the communicat or function
 		//The communicator function just runs the simple message
 		//send and receive functions.
+			
 			Client client = new Client();
 			client.Communicator();
 		
 	}
 }
-  
-
